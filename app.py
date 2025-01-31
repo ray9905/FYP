3f397e8c2a427a17a03d30d2b46e0ee527af5bc1
ray9905 <220105514@aston.ac.uk>
2025-01-31 23:57:32 +0000

    feat: add title and header to app

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Title
+st.title('Welcome to NeuroAI')
+
+# Header
+st.write('This application allows you to upload EEG data and outputs a text interpretation of the data.')
