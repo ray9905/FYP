8ea21ae32febeb41ab7fc7636121cc379866a3d1
ray9905 <220105514@aston.ac.uk>
2025-02-03 02:06:05 +0000

    feat: add numpy and panda libraries

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
+import pandas as pd 
+import numpy as np
 
 # Title
 st.title('Welcome to NeuroAI')
 
 # Header
-st.write('This application allows you to upload EEG data and outputs a text interpretation of the data.')
+
