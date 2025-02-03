f7886651f888e52c6028de3097fc876bc648e645
ray9905 <220105514@aston.ac.uk>
2025-02-03 16:39:42 +0000

    fix: api key integration

--- llm_text_summary.py
+++ llm_text_summary.py
@@ -1,10 +1,12 @@
 import os
 import openai
 
-gpt_key = os.getenv("OPENAI_KEY")
-
+client = openai.OpenAI(api_key= os.getenv("OPENAI_API_KEY"))
 
 
 def llm_text_summary(eeg_result):
     gpt_prompt = f"The EEG classification result is {eeg_result} summarise this result in simple terms."
-    return gpt_prompt
+    
+    
+                                     
+
