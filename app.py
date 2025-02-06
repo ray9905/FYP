f2a44a67d9dc65d715fa91e517404ea630f34f44
ray9905 <220105514@aston.ac.uk>
2025-02-06 12:16:12 +0000

    feat: added eeg file uploader , subheading and place holder for results

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ st.title('Welcome to NeuroAI')
 
 # Header
 st.write('Upload an EEG file to get started')
+
+eeg_file = st.file_uploader('Upload EEG file', type=['csv'])
+
+st.subheader('Results')
+st.write('intepretation of data from llm will be shown here as a summary')
