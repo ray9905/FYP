a5efca912b55254b28bf3a8936b34fb4523733ae
ray9905 <220105514@aston.ac.uk>
2025-02-06 12:18:10 +0000

    doc: add comments for recent changes

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ st.title('Welcome to NeuroAI')
 # Header
 st.write('Upload an EEG file to get started')
 
+#File uploader
 eeg_file = st.file_uploader('Upload EEG file', type=['csv'])
 
+#Subheading
 st.subheader('Results')
+
+#LLM summary
 st.write('intepretation of data from llm will be shown here as a summary')
