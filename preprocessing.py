a19d077b13c09fbccf65f1f74f3a5469dcb4e0d9
ray9905 <220105514@aston.ac.uk>
2025-02-10 00:35:21 +0000

    feat: add filter for eeg data

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,11 @@
 import mne
 
-#Loads the eeg data from edf file
+#loads the eeg data from edf file
 eeg_data = mne.io.read_raw_edf('', preload=True)
 
 #displays info about the eeg data
 print(eeg_data.info)
+
+#filters relevant data between 1 and 50 Hz
+eeg_data.filter(1, 50)
+
