80229a40ff794d957741579679381b5523c7fcfe
ray9905 <220105514@aston.ac.uk>
2025-02-10 19:27:28 +0000

    fix: transpose data and convert to microvolts

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 import mne
+import numpy as np
 
 #loads the eeg data from edf file
 eeg_data = mne.io.read_raw_edf('0000003.edf', preload=True)
@@ -11,4 +12,15 @@ eeg_data.filter(1, 50)
 
 #applies average reference to the data
 eeg_data.set_eeg_reference('average')
+
+#converts data from vaults to microvolts
+eeg_array = eeg_data.get_data() *1000000
+
+#swaps axes to samples, channels
+eeg_array = eeg_array.transpose(1,0)
+
+print(eeg_array)
+print(f"Minimum: {np.min(eeg_array)} , Maximum: {np.max(eeg_array)}")
+
+
 
