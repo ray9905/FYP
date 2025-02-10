477137ad3d7908813a0da4853354486e70d27380
ray9905 <220105514@aston.ac.uk>
2025-02-10 01:33:04 +0000

    test: added eeg file to test code

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,7 @@
 import mne
 
 #loads the eeg data from edf file
-eeg_data = mne.io.read_raw_edf('', preload=True)
+eeg_data = mne.io.read_raw_edf('0000003.edf', preload=True)
 
 #displays info about the eeg data
 print(eeg_data.info)
