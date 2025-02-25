bbbe9c91b02f86e0afaafb0c1a41fd76ffeaba16
ray9905 <220105514@aston.ac.uk>
2025-02-25 22:03:15 +0000

    feat: add CNN-LSTM model

--- model.py
+++ model.py
@@ -17,3 +17,34 @@ X_train, X_val, y_train, y_val = train_test_split(final_segment, labels, test_si
 #Print the shapes of the training and validation data
 print(f"Training Data Shape: {X_train.shape}, Training Labels Shape: {y_train.shape}")
 print(f"Validation Data Shape: {X_val.shape}, Validation Labels Shape: {y_val.shape}")
+
+#CNN-LSTM Model
+model = Sequential([
+
+    #First convolutional layer detects EEG patterns across different channels
+    Conv1D(64, kernel_size=3, activation='relu', input_shape=(X_train.shape[1], X_train.shape[2])),
+    
+    #First pooling layer reduces size 
+    MaxPooling1D(pool_size=2),
+
+    #Second convolutional layer extracts deeper EEG patterns
+    Conv1D(128, kernel_size=3, activation='relu'),
+
+    #Second pooling layer further reduces size
+    MaxPooling1D(pool_size=2),
+
+    #First LSTM layer detects temporal patterns in EEG data
+    LSTM(64, return_sequences=True),
+
+    #Second LSTM layer extracts deeper time dependent patterns in EEG data
+    LSTM(32),
+
+    #Fully connected dense layer converts EEG patterns into meaningful values
+    Dense(64, activation='relu'),
+
+    #Dropout Layer prevents overfitting
+    Dropout(0.3),
+
+    #Output Layer outputs 0 (normal) or 1 (abnormal)
+    Dense(1, activation='sigmoid')
+])
